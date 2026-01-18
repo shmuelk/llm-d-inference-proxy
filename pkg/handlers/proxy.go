f31673f94934b4ca9a6a1c8768276fa9430e2420
Shmuel Kallner <kallner@il.ibm.com>
2026-01-18 19:02:43 +0200

    Initial proxy code
    Signed-off-by: Shmuel Kallner <kallner@il.ibm.com>

---
+++ pkg/handlers/proxy.go
@@ -0,0 +1,158 @@
+package handlers
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+
+	"sigs.k8s.io/controller-runtime/pkg/log"
+	"sigs.k8s.io/gateway-api-inference-extension/pkg/epp/datastore"
+	giehandlers "sigs.k8s.io/gateway-api-inference-extension/pkg/epp/handlers"
+	"sigs.k8s.io/gateway-api-inference-extension/pkg/epp/metadata"
+	"sigs.k8s.io/gateway-api-inference-extension/pkg/epp/requestcontrol"
+	errutil "sigs.k8s.io/gateway-api-inference-extension/pkg/epp/util/error"
+	logutil "sigs.k8s.io/gateway-api-inference-extension/pkg/epp/util/logging"
+)
+
+const (
+	// defaultFairnessID is the default fairness ID used when no ID is provided in the request.
+	// This ensures that requests without explicit fairness identifiers are still grouped and managed by the Flow Control
+	// system.
+	defaultFairnessID = "default-flow"
+)
+
+func NewProxy(ds datastore.Datastore, director *requestcontrol.Director) http.Handler {
+	return &inferenceProxy{
+		ds:       ds,
+		director: director,
+	}
+}
+
+type inferenceProxy struct {
+	ds       datastore.Datastore
+	director *requestcontrol.Director
+}
+
+func (ip *inferenceProxy) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
+	ctx := req.Context()
+	logger := log.FromContext(ctx)
+
+	helper := inferenceProxyHelper{
+		ds:       ip.ds,
+		director: ip.director,
+		reqCtx: &giehandlers.RequestContext{
+			Request: &giehandlers.Request{
+				Headers:  make(map[string]string),
+				Body:     make(map[string]any),
+				Metadata: make(map[string]any),
+			},
+		},
+	}
+
+	err := prepareRequest(helper.reqCtx, req)
+	if err != nil {
+		sendError(err, rw)
+		return
+	}
+
+	helper.reqCtx, err = ip.director.HandleRequest(ctx, helper.reqCtx)
+	if err != nil {
+		logger.V(logutil.DEFAULT).Error(err, "Error handling request")
+		sendError(err, rw)
+		return
+	}
+
+	proxy := httputil.ReverseProxy{
+		Rewrite: helper.Rewrite,
+	}
+	proxy.ServeHTTP(rw, req)
+}
+
+type inferenceProxyHelper struct {
+	ds       datastore.Datastore
+	director *requestcontrol.Director
+	reqCtx   *giehandlers.RequestContext
+}
+
+// Rewrite is called by the ReverseProxy to rewite/redirect the request as needed
+func (ip *inferenceProxyHelper) Rewrite(pr *httputil.ProxyRequest) {
+	targetURL, _ := url.Parse("http://" + ip.reqCtx.TargetEndpoint)
+	pr.SetURL(targetURL)
+	ip.updateOutgoingRequest(pr.In.Context(), pr.Out)
+}
+
+func (ip *inferenceProxyHelper) updateOutgoingRequest(ctx context.Context, req *http.Request) {
+	requestBodyBytes, err := json.Marshal(ip.reqCtx.Request.Body)
+	if err != nil {
+		logger := log.FromContext(ctx)
+		logger.V(logutil.DEFAULT).Error(err, "Error marshalling request body")
+	}
+	// Update RequestSize to match marshalled body for Content-Length header.
+	ip.reqCtx.RequestSize = len(requestBodyBytes)
+	req.Body.Close()
+	req.Body = io.NopCloser(bytes.NewBuffer(requestBodyBytes))
+	req.ContentLength = int64(ip.reqCtx.RequestSize)
+	req.Header.Set("Content-Length", fmt.Sprintf("%d", ip.reqCtx.RequestSize))
+
+}
+
+func prepareRequest(reqCtx *giehandlers.RequestContext, req *http.Request) error {
+	ctx := req.Context()
+	logger := log.FromContext(ctx)
+	loggerTrace := logger.V(logutil.TRACE)
+	loggerTrace.Info("Processing")
+	for key, values := range req.Header {
+		reqCtx.Request.Headers[key] = values[0]
+		switch key {
+		case metadata.FlowFairnessIDKey:
+			reqCtx.FairnessID = reqCtx.Request.Headers[key]
+		case metadata.ObjectiveKey:
+			reqCtx.ObjectiveKey = reqCtx.Request.Headers[key]
+		case metadata.ModelNameRewriteKey:
+			reqCtx.TargetModelName = reqCtx.Request.Headers[key]
+		}
+	}
+
+	if reqCtx.FairnessID == "" {
+		reqCtx.FairnessID = defaultFairnessID
+	}
+
+	body, err := io.ReadAll(req.Body)
+	defer req.Body.Close()
+	if err != nil {
+		if logger.V(logutil.DEBUG).Enabled() {
+			logger.Info("Error unmarshaling request body", "body", string(body), "err", err)
+		}
+		err = errutil.Error{
+			Code: errutil.BadRequest,
+			Msg:  "Error unmarshaling request body",
+		}
+	} else {
+		loggerTrace.Info("Incoming body", "text", body)
+
+		loggerTrace.Info("decoding")
+		if errUnmarshal := json.Unmarshal(body, &reqCtx.Request.Body); errUnmarshal != nil {
+			if logger.V(logutil.DEBUG).Enabled() {
+				logger.Info("Error unmarshaling request body", "body", string(body), "err", errUnmarshal)
+			}
+			err = errutil.Error{
+				Code: errutil.BadRequest,
+				Msg:  "Error unmarshaling request body",
+			}
+		} else {
+			// Body stream complete. Capture raw size for flow control.
+			reqCtx.RequestSize = len(body)
+		}
+	}
+	return err
+}
+
+func sendError(err error, rw http.ResponseWriter) {
+	rw.WriteHeader(500)
+	rw.Write([]byte(err.Error()))
+}
---
+++ pkg/server/runserver.go
@@ -0,0 +1,137 @@
+package server
+
+import (
+	"context"
+	"crypto/tls"
+	"fmt"
+	"net"
+	"net/http"
+	"time"
+
+	"github.com/go-logr/logr"
+	"github.com/llm-d/llm-d-inference-proxy/pkg/handlers"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/manager"
+	"sigs.k8s.io/gateway-api-inference-extension/pkg/common"
+
+	backendmetrics "sigs.k8s.io/gateway-api-inference-extension/pkg/epp/backend/metrics"
+	dlmetrics "sigs.k8s.io/gateway-api-inference-extension/pkg/epp/datalayer/metrics"
+	"sigs.k8s.io/gateway-api-inference-extension/pkg/epp/datastore"
+	"sigs.k8s.io/gateway-api-inference-extension/pkg/epp/requestcontrol"
+	"sigs.k8s.io/gateway-api-inference-extension/pkg/epp/saturationdetector/framework/plugins/utilizationdetector"
+)
+
+type HttpServerRunner struct {
+	GrpcPort                         int
+	GKNN                             common.GKNN
+	Datastore                        datastore.Datastore
+	SecureServing                    bool
+	HealthChecking                   bool
+	CertPath                         string
+	EnableCertReload                 bool
+	RefreshPrometheusMetricsInterval time.Duration
+	MetricsStalenessThreshold        time.Duration
+	Director                         *requestcontrol.Director
+	SaturationDetector               *utilizationdetector.Detector
+	UseExperimentalDatalayerV2       bool // Pluggable data layer feature flag
+
+	// This should only be used in tests. We won't need this once we do not inject metrics in the tests.
+	// TODO:(https://github.com/kubernetes-sigs/gateway-api-inference-extension/issues/432) Cleanup
+	TestPodMetricsClient *backendmetrics.FakePodMetricsClient
+}
+
+func (r *HttpServerRunner) AsRunnable(logger logr.Logger) manager.Runnable {
+	return NoLeaderElection(manager.RunnableFunc(func(ctx context.Context) error {
+		if r.UseExperimentalDatalayerV2 {
+			dlmetrics.StartMetricsLogger(ctx, r.Datastore, r.RefreshPrometheusMetricsInterval, r.MetricsStalenessThreshold)
+		} else {
+			backendmetrics.StartMetricsLogger(ctx, r.Datastore, r.RefreshPrometheusMetricsInterval, r.MetricsStalenessThreshold)
+		}
+
+		srv := &http.Server{}
+		if r.SecureServing {
+			var cert tls.Certificate
+			var err error
+			if r.CertPath != "" {
+				cert, err = tls.LoadX509KeyPair(r.CertPath+"/tls.crt", r.CertPath+"/tls.key")
+			} else {
+				// Create tls based credential.
+				cert, err = CreateSelfSignedTLSCertificate(logger)
+			}
+			if err != nil {
+				return fmt.Errorf("failed to create self signed certificate - %w", err)
+			}
+
+			var tlsConfig *tls.Config
+			if r.CertPath != "" && r.EnableCertReload {
+				reloader, err := common.NewCertReloader(ctx, r.CertPath, &cert)
+				if err != nil {
+					return fmt.Errorf("failed to create cert reloader: %w", err)
+				}
+				tlsConfig = &tls.Config{
+					GetCertificate: func(_ *tls.ClientHelloInfo) (*tls.Certificate, error) {
+						return reloader.Get(), nil
+					},
+				}
+			} else {
+				tlsConfig = &tls.Config{
+					Certificates: []tls.Certificate{cert},
+				}
+			}
+			srv.TLSConfig = tlsConfig
+		}
+
+		srv.Handler = handlers.NewProxy(r.Datastore, r.Director)
+
+		// Forward to the Http runnable.
+		return HttpServer("http-src", srv, r.GrpcPort, r.SecureServing).Start(ctx)
+	}))
+}
+
+// HttpServer converts the given Httpserver into a runnable.
+// The server name is just being used for logging.
+func HttpServer(name string, srv *http.Server, port int, usingTLS bool) manager.Runnable {
+	return manager.RunnableFunc(func(ctx context.Context) error {
+		// Use "name" key as that is what manager.Server does as well.
+		log := ctrl.Log.WithValues("name", name)
+		log.Info("Http server starting")
+
+		srv.BaseContext = func(net.Listener) context.Context {
+			return ctx
+		}
+
+		// Start listening.
+		lis, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
+		if err != nil {
+			return fmt.Errorf("Http server failed to listen - %w", err)
+		}
+
+		log.Info("Http server listening", "port", port)
+
+		// Shutdown on context closed.
+		// Terminate the server on context closed.
+		// Make sure the goroutine does not leak.
+		doneCh := make(chan struct{})
+		defer close(doneCh)
+		go func() {
+			select {
+			case <-ctx.Done():
+				log.Info("Http server shutting down")
+				srv.Shutdown(context.Background())
+			case <-doneCh:
+			}
+		}()
+
+		// Keep serving until terminated.
+		if usingTLS {
+			err = srv.ServeTLS(lis, "", "")
+		} else {
+			err = srv.Serve(lis)
+		}
+		if err != nil && err != http.ErrServerClosed {
+			return fmt.Errorf("Http server failed - %w", err)
+		}
+		log.Info("Http server terminated")
+		return nil
+	})
+}
